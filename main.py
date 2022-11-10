93298cf9b9d0b5cd48ed6c6d78a402d0cc632b85
Лилия <l_loshakova123@mail.ru>
2022-11-11 00:43:22 +0300

    Outline of the solution

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-
+import datetime
 import bot_commands as bc
 
-updater = Updater('5662038385:AAF2I6d4wI78B4jOQ9Erkb_U7-uPBBnDyGo')
+updater = Update("5662038385:AAF2I6d4wI78B4jOQ9Erkb_U7-uPBBnDyGo")
 updater.dispatcher.add_handler(CommandHandler('hi', bc.hi_comand))
 updater.dispatcher.add_handler(CommandHandler('time', bc.time_command))
 updater.dispatcher.add_handler(CommandHandler('help', bc.help_command))
 updater.dispatcher.add_handler(CommandHandler('sum', bc.sum_command))
 
+def hi_comand(update: Update, context: CallbackContext):
+    update.message.reply_text(f'Hi{update.effective_user.first_name}!')
+
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text(f'/hi\n/time\n/help')
+
+def time_command(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{datetime.datetime.now().time()}')
 
 
 print('server start')
 updater.start_polling()
 updater.idle()
 
-def log(update: Update, context: CallbackContext):
- file = open('db.csv', 'a')
- file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}\n')
- file.close()
+# def log(update: Update, context: CallbackContext):
+#  file = open('db.csv', 'a')
+#  file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}\n')
+#  file.close()
