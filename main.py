2b8b52123bc0e9ef1389af55916c949e96761be3
Лилия <l_loshakova123@mail.ru>
2022-11-11 13:47:09 +0300

    New file model

--- main.py
+++ main.py
@@ -30,7 +30,3 @@ bc.bot.polling(none_stop=True, interval=0)
 # updater.start_polling()
 # updater.idle()
 
-# def log(update: Update, context: CallbackContext):
-#  file = open('db.csv', 'a')
-#  file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}\n')
-#  file.close()
